ef9ecee3487e2171847b9e05e080cf1a29d523a0
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-03-12 21:45:22 +0900

    Fix path

--- ComfyUI-fastapi/tests/test_workflow_executor.py
+++ ComfyUI-fastapi/tests/test_workflow_executor.py
@@ -9,8 +9,7 @@ from .conftest import comfyui_fastapi_dir
 
 def test_load_workflow():
     """Test loading a valid workflow file"""
-    print("\n\n\n\ncomfyui_fastapi_dir: ", comfyui_fastapi_dir, "\n\n\n\n")
-    workflow_path = Path(os.path.join(comfyui_fastapi_dir, "workflows/basic.json"))
+    workflow_path = Path(os.path.abspath(os.path.join(comfyui_fastapi_dir, "workflows","basic.json")))
     assert workflow_path.exists(), "Workflow file does not exist"
 
     executor = WorkflowExecutor(workflow_path)
